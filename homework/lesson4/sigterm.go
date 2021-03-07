7e839c189d47a112f13ac9d46ab8b3fc03c20860
Yevgeniy <evgeniyv6@gmail.com>
2021-03-07 23:07:37 +0300

    dev ctx sigterm

--- homework/lesson4/sigterm.go
+++ homework/lesson4/sigterm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -38,8 +39,54 @@ func SigtermExtTimeout() {
 		done <- true
 	}()
 	fmt.Printf("graceful exit - %v, after %d sec", <-done, timeout)
+
+	time.Sleep(25*time.Second)
+}
+
+
+func SigtermExtTimeout2() {
+	var (
+		ctx, cancel = context.WithCancel(context.Background())
+		cancelSig = make(chan os.Signal)
+		workers = make(chan struct{})
+		done = make(chan bool,1)
+
+		doWork = func(num int) {
+			for i := 0; i < num; i++ {
+				workers <- struct{}{}
+				time.Sleep(1 * time.Second)
+			}
+		}
+
+		getWork = func() {
+			for {
+				fmt.Println("get from worker", <- workers)
+			}
+		}
+
+		catchSIG = func(cancel context.CancelFunc) {
+			sig := <-cancelSig
+			fmt.Println("caugth sigterm", sig)
+			done <- true
+			cancel()
+		}
+	)
+
+	signal.Notify(cancelSig, syscall.SIGTERM, syscall.SIGINT)
+
+	go doWork(100)
+	go getWork()
+	go catchSIG(cancel)
+
+	select {
+	case <-ctx.Done():
+		time.Sleep(timeout*time.Second)
+		fmt.Println("Completed.", <-done)
+		return
+	}
+
 }
 
 func main() {
-	SigtermExtTimeout()
+	SigtermExtTimeout2()
 }
