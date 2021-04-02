668fc6478c30bb11f1c6bf98c11a05391964702e
Yevgeniy <evgeniyv6@gmail.com>
2021-04-02 14:32:32 +0300

    correct map key with files hash

--- homework/lesson8/duplicates/duplicates.go
+++ homework/lesson8/duplicates/duplicates.go
@@ -51,7 +51,7 @@ func customWalkDirFunc(ch chan FileStat, wg *sync.WaitGroup) func(string, os.Dir
 		// 2) ModeType: это "Mask for the type bits" ->
 		// ModeDir|ModeSymlink|ModeNamedPipe|ModeSocket|ModeDevice|ModeCharDevice|ModeIrregular
 		// т.о. если нет такого бита - то это файл, что нам и нужно
-		if err == nil && info.Size() > 0 && (entry.Type() & os.ModeType == 0) {
+		if err == nil && info.Size() > 0 && (entry.Type()&os.ModeType == 0) {
 			wg.Add(1)
 			go processFile(path, info, ch, wg)
 		}
@@ -80,14 +80,14 @@ func processFile(file string, info os.FileInfo, ch chan FileStat, wg *sync.WaitG
 		return
 	}
 
-	ch <- FileStat{hash.Sum(nil),info.Size(), file}
+	ch <- FileStat{hash.Sum(nil), info.Size(), file}
 }
 
 func MapResults(ch <-chan FileStat) map[string]*Paths {
 	mm := make(map[string]*Paths)
 	format := fmt.Sprintf("%%016X:%%%dX", crc32.Size*2) // == "%016X:%40X"
 	for msg := range ch {
-		key:= fmt.Sprintf(format, msg.Size, msg.Hash)
+		key := fmt.Sprintf(format, msg.Size, msg.Hash)
 		val, ok := mm[key]
 		if !ok {
 			val = &Paths{}
@@ -102,16 +102,17 @@ func ResultWorker(mm map[string]*Paths, clear bool) (toRemove []string) {
 	for _, val := range mm {
 		if len(*val) > 1 {
 			fmt.Printf("# number of duplicates - %d, see the list below:\n", len(*val))
-			sort.Slice(*val, func(i, j int) bool {  // сортируем по ctime
+			sort.Slice(*val, func(i, j int) bool { // сортируем по ctime
 				f1, err := os.Stat((*val)[i])
 				if err != nil {
-					fmt.Printf("cannot get file stat %s. Skip.\n",(*val)[i])
+					fmt.Printf("cannot get file stat %s. Skip.\n", (*val)[i])
 				}
-				f2, err :=os.Stat((*val)[j])
+				f2, err := os.Stat((*val)[j])
 				if err != nil {
-					fmt.Printf("cannot get file stat %s. Skip.\n",(*val)[j])
+					fmt.Printf("cannot get file stat %s. Skip.\n", (*val)[j])
 				}
-				ctimef1 := f1.Sys().(*syscall.Stat_t).Ctimespec; ctimef2 := f2.Sys().(*syscall.Stat_t).Ctimespec
+				ctimef1 := f1.Sys().(*syscall.Stat_t).Ctimespec
+				ctimef2 := f2.Sys().(*syscall.Stat_t).Ctimespec
 				return timespecToTime(ctimef1).Unix() < timespecToTime(ctimef2).Unix()
 				// return len((*val)[i]) < len((*val)[j]) // либо сортируем по короткому пути, в зависимости от ТЗ,
 				// либо добавить признак выбора сортировки для пользователя - TBD
@@ -120,7 +121,7 @@ func ResultWorker(mm map[string]*Paths, clear bool) (toRemove []string) {
 			for _, file := range (*val)[1:] {
 				fmt.Printf("\t%s\n", file)
 				if clear {
-					toRemove = append(toRemove,file)
+					toRemove = append(toRemove, file)
 				}
 			}
 		}
@@ -140,10 +141,10 @@ func RemoveDuplicates(files []string) {
 			defer wg.Done()
 			mu.Lock()
 			err := os.Remove(f)
-			if err !=nil {
-				fmt.Printf("Cannot remove file %s. Skip.\n",f)
+			if err != nil {
+				fmt.Printf("Cannot remove file %s. Skip.\n", f)
 			} else {
-				fmt.Printf("File deleted: %s.\n",f)
+				fmt.Printf("File deleted: %s.\n", f)
 			}
 			mu.Unlock()
 		}(file)
